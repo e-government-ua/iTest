e4ebaf2c81a7d0ebe7cf540e5e2547b9bb28043d
Denys Lapin <dlapin@softservecom.com>
2016-02-22 18:35:00 +0200

    added class AppealToHeadOfRegion and test case

---
+++ src/main/java/pages/service/authorities/interaction/AppealToHeadOfRegion.java
@@ -0,0 +1,7 @@
+package pages.service.authorities.interaction;
+
+/**
+ * Created by dlapin on 2/22/2016.
+ */
+public class AppealToHeadOfRegion {
+}
---
+++ src/test/java/test/portal/services/authorities/interaction/AppealToHeadOfregion.java
@@ -0,0 +1,7 @@
+package test.portal.services.authorities.interaction;
+
+/**
+ * Created by dlapin on 2/22/2016.
+ */
+public class AppealToHeadOfregion {
+}
